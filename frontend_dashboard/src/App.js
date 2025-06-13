f635b6ded7578f41a17899f9759f76265c2b4733
Christoph <c.wagner.germany@gmail.com>
2025-06-13 17:00:30 +0200

    topbar

--- frontend_dashboard/src/App.js
+++ frontend_dashboard/src/App.js
@@ -1,5 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, Themeprovider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
