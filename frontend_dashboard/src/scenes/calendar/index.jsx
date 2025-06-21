81f67b62232718ec34f51c3a06a17171c79ea840
Christoph <c.wagner.germany@gmail.com>
2025-06-21 15:19:21 +0200

    cal

---
+++ frontend_dashboard/src/scenes/calendar/index.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+const Calendar = () => {
+
+}
+
+export default Calendar;
