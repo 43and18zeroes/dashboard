a58f76d58c565684d00e0dc1436de0d19786ca13
Christoph <c.wagner.germany@gmail.com>
2025-06-25 15:46:35 +0200

    stat

--- frontend_dashboard/src/components/Statbox.jsx
+++ frontend_dashboard/src/components/Statbox.jsx
@@ -22,11 +22,16 @@ const Statbox = ({ title, subtitle, icon, progress, increase }) => {
           <ProgressCircle progress={progress} />
         </Box>
         <Box display="flex" justifyContent="space-between">
-            <Typography variant="h5" sx={{ color: colors.greenAccent[100] }}>
-                {title}
-            </Typography>
+          <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+            {subtitle}
+          </Typography>
+          <Typography variant="h5" fontStyle="italic" sx={{ color: colors.greenAccent[600] }}>
+            {increase}
+          </Typography>
         </Box>
       </Box>
     </Box>
   );
 };
+
+export default Statbox;
