93ed626bc1e458ee60ffdd872dca1e15a6e219d4
Christoph <c.wagner.germany@gmail.com>
2025-06-27 15:49:40 +0200

    misc2

--- frontend_dashboard/src/scenes/dashboard/index.jsx
+++ frontend_dashboard/src/scenes/dashboard/index.jsx
@@ -244,6 +244,43 @@ const Dashboard = () => {
             <Typography>Includes extra misc expenditures and costs</Typography>
           </Box>
         </Box>
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ p: "30px 30px 0 30px" }}
+          >
+            Sales Quantity
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+
+                <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          p="30px"
+        >
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ mb: "15px" }}
+          >
+            Sales Quantity
+          </Typography>
+          <Box height="200">
+            <GeographyChart isDashboard={true} />
+          </Box>
+        </Box>
+
+          {/*   */}
+
       </Box>
     </Box>
   );
